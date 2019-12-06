49fa99b00eda65a26cfa4cf85e16f7bc9f273d76
Aditya Pandey <Aditya.Pandey@aptushealth.com>
2019-12-06 21:38:13 +0530

    First push

---
+++ views/site/index.php
@@ -0,0 +1,53 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'ClearUrGST';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Welcome to ClearUrGST!</h1>
+
+        <p class="lead">We provide a wide range of services in Tax, Audit, Accounting, GST etc.</p>
+
+        <p><a class="btn btn-lg btn-success" href="#">Enquire Now</a></p>
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-4">
+                <h2>ITR Filing</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-default" href="#">Enquire Now &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>GST Consultation</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-default" href="#">Enquire Now &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Tax Advice</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-default" href="#">Enquire Now &raquo;</a></p>
+            </div>
+        </div>
+
+    </div>
+</div>
---
+++ views/site/services.php
@@ -0,0 +1,22 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'Services';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        Below are the some of the services which we provide.
+        <ul>
+            <li>IT Return Filing</li>
+            <li>Tax Consultation</li>
+            <li>GST Consultation</li>
+            <li>Accounting</li>
+        </ul>
+    </p>
+</div>
